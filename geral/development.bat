d8f48a9977c6ea7e007dd341e45c24d64fb9f6ed
victor oliveira <victorfrco3@hotmail.com>
2017-12-23 20:08:47 -0200

    tabaca commit

--- geral/development.bat
+++ geral/development.bat
@@ -2,6 +2,6 @@
 start C:\starthttpd.vbs
 start C:\startmysqld.vbs
 start chrome http://localhost:8000
-cd \Users\victor.oliveira\ProjetoBatata\
+cd \Users\Federal Hookah Pub\federalHookah\federalHookah
 php artisan serve
 Exit
