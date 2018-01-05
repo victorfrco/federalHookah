88e947072ad3b798acce7ec468960474f86aaf89
victor.oliveira <chico1996>
2018-01-05 18:37:47 -0300

    Ajustes iniciais

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -45,8 +45,8 @@ class DatabaseSeeder extends Seeder
 		
 		DB::table('brands')->insert([
             'name' =>'Heineken',
-            'description' => 'Be Legacy!',
-            'logo_path' => 'https://seeklogo.com/images/S/stella-artois-logo-04BFEE2241-seeklogo.com.png',
+            'description' => 'Open your World!',
+            'logo_path' => 'storage/images/brands/Heineken.jpg',
             'status' => 1,
             'category_id' => 1
         ]);
@@ -54,7 +54,7 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Stella Artois',
             'description' => 'Be Legacy!',
-            'logo_path' => 'https://seeklogo.com/images/S/stella-artois-logo-04BFEE2241-seeklogo.com.png',
+            'logo_path' => 'storage/images/brands/Stella Artois.png',
             'status' => 1,
             'category_id' => 1
         ]);
@@ -62,7 +62,7 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Antarctica Original',
             'description' => 'É difícil achar, mas vale a pena!',
-            'logo_path' => 'http://viracoposlanchonete.com.br/images/LOGO-BEBIDAS/ORIGINAL.png',
+            'logo_path' => 'storage/images/brands/Antarctica Original.png',
             'status' => 1,
             'category_id' => 1
         ]);
@@ -70,7 +70,7 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Coca-Cola',
             'description' => 'Abra a felicidade!',
-            'logo_path' => 'https://seeklogo.com/images/C/Coca-Cola-logo-00A6B20F2F-seeklogo.com.png',
+            'logo_path' => 'storage/images/brands/Coca-Cola.png',
             'status' => 1,
             'category_id' => 1
         ]);
@@ -78,91 +78,91 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Guarana Antarctica',
             'description' => 'O original do Brasil',
-            'logo_path' => 'https://pedidodaweb.com.br/upload/1501087688-GUARANA.png',
+            'logo_path' => 'storage/images/brands/Guarana Antarctica.png',
             'status' => 1,
             'category_id' => 1
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Zomo',
-            'logo_path' => 'https://www.pegasustabacaria.com.br/wp-content/uploads/2017/11/logozomo.png',
+            'logo_path' => 'storage/images/brands/Zomo.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Nay',
-            'logo_path' => 'http://centraldonarguile.com/wp-content/uploads/2017/10/nay.png',
+            'logo_path' => 'storage/images/brands/Nay.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'VGod',
-            'logo_path' => 'https://xlvape.com/media/wysiwyg/vgod-eliterda/elite-rda-logo-white.png',
+            'logo_path' => 'storage/images/brands/VGod.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Adalya',
-            'logo_path' => 'http://reidatabacaria.vteximg.com.br/arquivos/ids/155792-558-554/adalya1.png',
+            'logo_path' => 'storage/images/brands/Adalya.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Haze',
-            'logo_path' => 'http://images.tcdn.com.br/img/img_prod/86670/haze_tobacco_250g_148_1_20170307173116.png',
+            'logo_path' => 'storage/images/brands/Haze.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Fumari',
-            'logo_path' => 'https://i2.wp.com/hookahdihufa.com/wp-content/uploads/2015/03/fumari_logo_grey.png',
+            'logo_path' => 'storage/images/brands/Fumari.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Tangiers',
-            'logo_path' => 'http://images.tcdn.com.br/img/img_prod/86670/tangiers_tobacco_250g_86_1_20160928173330.png',
+            'logo_path' => 'storage/images/brands/Tangiers.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Pure Tobacco',
-            'logo_path' => 'http://www.emporionb.com.br/uploads/thumb-pure-morangie--250g_1477278708960480910.png',
+            'logo_path' => 'storage/images/brands/Pure Tobacco.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Atcha',
-            'logo_path' => 'https://scontent.fgig1-2.fna.fbcdn.net/v/t1.0-9/13006759_1005994996159212_6112715578030064016_n.jpg?oh=1bb58a06a43fc092767a0c53640c9e92&oe=5AD194FF',
+            'logo_path' => 'storage/images/brands/Atcha.jpg',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Mazaya',
-            'logo_path' => 'https://misternarguile.com.br/wp-content/uploads/2016/11/Mazaya-1.png',
+            'logo_path' => 'storage/images/brands/Mazaya.png',
             'status' => 1,
             'category_id' => 3
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Dichavadores',
-            'logo_path' => 'http://rolliepipe.com.br/wp-content/uploads/2017/09/Graphic1-550x550-300x300.png',
+            'logo_path' => 'storage/images/brands/Dichavadores.png',
             'status' => 1,
             'category_id' => 4
         ]);
 
         DB::table('brands')->insert([
             'name' =>'Doces e Aperitivos',
-            'logo_path' => 'http://newflashmotel.com.br/public/imagens/cardapio/diversos.png',
+            'logo_path' => 'storage/images/brands/Doces e Aperitivos.png',
             'status' => 1,
             'category_id' => 4
         ]);
@@ -170,7 +170,7 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Rosh Simples',
             'description' => 'rosh feito com fumos simples',
-            'logo_path' => 'http://ap.imagensbrasil.org/images/2017/12/15/logosimples.png',
+            'logo_path' => 'storage/images/brands/Rosh Simples.png',
             'status' => 1,
             'category_id' => 2
         ]);
@@ -178,7 +178,7 @@ class DatabaseSeeder extends Seeder
         DB::table('brands')->insert([
             'name' =>'Rosh Premium',
             'description' => 'rosh feito com fumos premium',
-            'logo_path' => 'http://www.makebrandz.com/wp-content/uploads/2015/01/Premium-Quality-02_300x162.png',
+            'logo_path' => 'storage/images/brands/Rosh Premium.png',
             'status' => 1,
             'category_id' => 2
         ]);
@@ -186,7 +186,7 @@ class DatabaseSeeder extends Seeder
 	    DB::table('brands')->insert([
 		    'name' =>'Corona',
 		    'description' => 'A cerveja mexincana!',
-		    'logo_path' => 'https://hedstrom.com/wp-content/uploads/2016/09/Corona-Logo.jpg',
+		    'logo_path' => 'storage/images/brands/Corona.jpg',
 		    'status' => 1,
 		    'category_id' => 1
 	    ]);
@@ -194,7 +194,7 @@ class DatabaseSeeder extends Seeder
 	    DB::table('brands')->insert([
 		    'name' =>'Combos',
 		    'description' => 'Combos de bebidas',
-		    'logo_path' => 'http://nopontodistribuidora.com.br/wp-content/uploads/2016/05/COMBO-01.png',
+		    'logo_path' => 'storage/images/brands/Combos.png',
 		    'status' => 1,
 		    'category_id' => 1
 	    ]);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Batata+</title>
+    <title>Federal Hookah Pub</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -78,7 +78,7 @@
 
     <div class="content">
         <div class="title m-b-md">
-            Batata+
+            Federal Hookah Pub
         </div>
     </div>
 </div>
